714c3769ccada991991a7d3d68f18a96f7f0ad47
Macir Junior <mj.brustolin@hotmail.com>
2022-01-06 15:39:50 -0300

    Adicionar arquivos de projeto.

---
+++ EstrelaDaMorte_WindowsForms/Extensions/ValueExtention.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.SqlClient;
+
+namespace EstrelaDaMorte_WindowsForms.Extensions {
+    public static class ValueExtension {
+
+        public static T GetValueOrDefault<T>(this SqlDataReader reader, string campo) {
+            try {
+                return (T)reader[campo];
+            } catch {
+                return default(T);
+            }
+        }
+
+    }
+}
---
+++ EstrelaDaMorte_WindowsForms/ViewModels/ResultadoApi.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace EstrelaDaMorte_WindowsForms.ViewModels {
+    public class ResultadoApi<ViewModel> {
+        public string Next { get; set; }
+        public IReadOnlyList<ViewModel> Results { get; set; }
+    }
+}
